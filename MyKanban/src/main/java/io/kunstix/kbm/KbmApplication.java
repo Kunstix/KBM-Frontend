a9d97a5fc8728285fe0cc769b7917bcf041f2999
kunstix <scroovy@web.de>
2020-08-07 19:55:44 +0200

    setup git for kanbanmanager

---
+++ MyKanban/src/main/java/io/kunstix/kbm/KbmApplication.java
@@ -0,0 +1,13 @@
+package io.kunstix.kbm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbmApplication.class, args);
+    }
+
+}
---
+++ MyKanban/src/test/java/io/kunstix/kbm/KbmApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kunstix.kbm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
