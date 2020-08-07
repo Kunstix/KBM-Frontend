27d94793bafa4e2047adf66e960e3b00c4d0057d
kunstix <scroovy@web.de>
2020-08-07 22:15:58 +0200

    setup project pojo service controller

---
+++ MyKanban/src/main/java/io/kunstix/kbm/domain/Project.java
@@ -0,0 +1,116 @@
+package io.kunstix.kbm.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Project name is required")
+    private String projectName;
+
+    @NotBlank(message = "ProjectID name is required")
+    @Size(min=4, max=5, message = "Length between 4 to 5 required")
+    @Column(updatable = false, unique = true)
+    private String projectID;
+
+    @NotBlank(message = "Description required")
+    private String description;
+
+    @JsonFormat(pattern ="yyyy-mm-dd")
+    private Date startDate;
+
+    @JsonFormat(pattern ="yyyy-mm-dd")
+    private Date endDate;
+
+    @JsonFormat(pattern ="yyyy-mm-dd")
+    private Date createdAt;
+
+    @JsonFormat(pattern ="yyyy-mm-dd")
+    private Date updatedAt;
+
+    public Project() {
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected  void onUpdate() {
+        this.updatedAt = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectID() {
+        return projectID;
+    }
+
+    public void setProjectID(String projectID) {
+        this.projectID = projectID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ MyKanban/src/main/java/io/kunstix/kbm/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package io.kunstix.kbm.repositories;
+
+import io.kunstix.kbm.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+
+
+}
